d37aa1971fcc59da8f89127b28ba3ea5b3693f55
Maksym Subchak <donjkee2@gmail.com>
2025-08-09 17:58:48 +0300

    feat: Add models for todo app and configure Django admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Task, Tag
+
+admin.site.register(Tag)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "datetime",
+        "deadline",
+        "is_complete",
+    )
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_complete = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
